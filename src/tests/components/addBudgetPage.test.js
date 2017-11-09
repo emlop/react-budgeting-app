7d1a44cbfa2e3791a06a738cba7ae46604a34ea6
Emel <emel@Emels-MacBook-Air.local>
2017-11-09 01:22:41 -0500

    Setup env environment

--- src/tests/components/addBudgetPage.test.js
+++ src/tests/components/addBudgetPage.test.js
@@ -3,12 +3,12 @@ import { shallow } from 'enzyme';
 import { AddBudgetPage } from '../../components/AddBudgetPage';
 import expenses from '../fixtures/expenses';
 
-let addExpense, history, wrapper;
+let startAddExpense, history, wrapper;
 
 beforeEach(() => {
-  addExpense = jest.fn();
+  startAddExpense = jest.fn();
   history = { push: jest.fn() };
-  wrapper = shallow(<AddBudgetPage addExpense={addExpense} history={history} />);
+  wrapper = shallow(<AddBudgetPage startAddExpense={startAddExpense} history={history} />);
 });
 
 test('should render AddBudgetPage correctly', () => {
@@ -18,5 +18,5 @@ test('should render AddBudgetPage correctly', () => {
 test('should handle onSubmit', () => {
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
   expect(history.push).toHaveBeenLastCalledWith('/');
-  expect(addExpense).toHaveBeenLastCalledWith(expenses[1]);
+  expect(startAddExpense).toHaveBeenLastCalledWith(expenses[1]);
 });
