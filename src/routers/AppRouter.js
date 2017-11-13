456d4d9bc003f3d09376cde503275465873d25a1
Emel <emel@Emels-MacBook-Air.local>
2017-11-13 03:17:26 -0500

    Setting up auth

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 import AddBudgetPage from '../components/AddBudgetPage';
 import BudgetDashboardPage from '../components/BudgetDashboardPage';
 import EditBudgetPage from '../components/EditBudgetPage';
-import Header from '../components/Header';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
+import LoginPage from '../components/LoginPage';
+import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
+
+export const history = createHistory();
 
 const AppRouter = () => (
-  <BrowserRouter>
+  <Router history={history}>
     <div>
-      <Header />
       <Switch>
-        <Route path="/" component={BudgetDashboardPage} exact={true} />
-        <Route path="/create" component={AddBudgetPage} />
-        <Route path="/edit/:id" component={EditBudgetPage} />
+        <PublicRoute path="/" component={LoginPage} exact={true} />
+        <PrivateRoute path="/dashboard" component={BudgetDashboardPage} />
+        <PrivateRoute path="/create" component={AddBudgetPage} />
+        <PrivateRoute path="/edit/:id" component={EditBudgetPage} />
         <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch> 
     </div> 
-  </BrowserRouter>
+  </Router>
 );
 
 export default AppRouter;
